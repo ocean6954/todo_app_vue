d73de1b344c8a978de866338e394fb493f640ada
ocean6954 <oceanan699@gmail.com>
2025-10-03 17:50:16 +0900

    todo作成と削除機能

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Todo } from "../types/todo";
+
+const props = defineProps<{
+  todo: Todo;
+  index: number;
+}>();
+
+const emit = defineEmits<{
+  (e: "remove-todo", index: number): void;
+}>();
+</script>
+
+<template>
+  <li>
+    <span>{{ props.todo.text }}</span>
+    <button @click="emit('remove-todo', index)">Delete</button>
+  </li>
+</template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Todo } from "../types/todo.ts";
+import TodoItem from "./TodoItem.vue";
+
+// Todoリスト
+const props = defineProps<{
+  todos: Todo[];
+}>();
+
+const emit = defineEmits<{
+  (e: "remove-todo", index: number): void;
+}>();
+</script>
+
+<template>
+  <ul>
+    <TodoItem
+      v-for="(todo, index) in props.todos"
+      :key="todo.id"
+      :todo="todo"
+      :index="index"
+      @remove-todo="emit('remove-todo', $event)"
+    />
+  </ul>
+</template>
